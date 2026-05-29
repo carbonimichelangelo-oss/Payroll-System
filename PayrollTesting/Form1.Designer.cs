4f27e523b7fb9cd297301fb92574e0ffa69a2f40
MichelangeloSSC <michelangeloc@thessc.co.za>
2026-05-29 22:12:42 +0200

    first commit

---
+++ PayrollTesting/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace PayrollTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contractorName = new System.Windows.Forms.TextBox();
+            this.hoursWorked = new System.Windows.Forms.TextBox();
+            this.dependants = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.grossPay = new System.Windows.Forms.TextBox();
+            this.payeDeduction = new System.Windows.Forms.TextBox();
+            this.uifDeduction = new System.Windows.Forms.TextBox();
+            this.membershipFee = new System.Windows.Forms.TextBox();
+            this.totalDeduction = new System.Windows.Forms.TextBox();
+            this.netPay = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(43, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mzansi Tech Contractors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(89, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contractor name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(114, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hours worked:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(36, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number of dependants:";
+            // 
+            // contractorName
+            // 
+            this.contractorName.Location = new System.Drawing.Point(271, 123);
+            this.contractorName.Name = "contractorName";
+            this.contractorName.Size = new System.Drawing.Size(147, 22);
+            this.contractorName.TabIndex = 4;
+            // 
+            // hoursWorked
+            // 
+            this.hoursWorked.Location = new System.Drawing.Point(271, 181);
+            this.hoursWorked.Name = "hoursWorked";
+            this.hoursWorked.Size = new System.Drawing.Size(147, 22);
+            this.hoursWorked.TabIndex = 5;
+            // 
+            // dependants
+            // 
+            this.dependants.Location = new System.Drawing.Point(271, 235);
+            this.dependants.Name = "dependants";
+            this.dependants.Size = new System.Drawing.Size(147, 22);
+            this.dependants.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(621, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Gross Pay:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(580, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "PAYE deduction:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(596, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "UIF deduction:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(580, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Membership fee:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Location = new System.Drawing.Point(585, 290);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(128, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Total deduction:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Location = new System.Drawing.Point(640, 326);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 20);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Net Pay:";
+            // 
+            // grossPay
+            // 
+            this.grossPay.Location = new System.Drawing.Point(740, 119);
+            this.grossPay.Name = "grossPay";
+            this.grossPay.Size = new System.Drawing.Size(147, 22);
+            this.grossPay.TabIndex = 13;
+            // 
+            // payeDeduction
+            // 
+            this.payeDeduction.Location = new System.Drawing.Point(740, 162);
+            this.payeDeduction.Name = "payeDeduction";
+            this.payeDeduction.Size = new System.Drawing.Size(147, 22);
+            this.payeDeduction.TabIndex = 14;
+            // 
+            // uifDeduction
+            // 
+            this.uifDeduction.Location = new System.Drawing.Point(740, 205);
+            this.uifDeduction.Name = "uifDeduction";
+            this.uifDeduction.Size = new System.Drawing.Size(147, 22);
+            this.uifDeduction.TabIndex = 15;
+            // 
+            // membershipFee
+            // 
+            this.membershipFee.Location = new System.Drawing.Point(740, 246);
+            this.membershipFee.Name = "membershipFee";
+            this.membershipFee.Size = new System.Drawing.Size(147, 22);
+            this.membershipFee.TabIndex = 16;
+            // 
+            // totalDeduction
+            // 
+            this.totalDeduction.Location = new System.Drawing.Point(740, 288);
+            this.totalDeduction.Name = "totalDeduction";
+            this.totalDeduction.Size = new System.Drawing.Size(147, 22);
+            this.totalDeduction.TabIndex = 17;
+            // 
+            // netPay
+            // 
+            this.netPay.Location = new System.Drawing.Point(740, 324);
+            this.netPay.Name = "netPay";
+            this.netPay.Size = new System.Drawing.Size(147, 22);
+            this.netPay.TabIndex = 18;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(50, 290);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(157, 36);
+            this.calculateBtn.TabIndex = 19;
+            this.calculateBtn.Text = "Calculate Net Pay";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(214, 290);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(99, 36);
+            this.resetBtn.TabIndex = 20;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(319, 290);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(99, 36);
+            this.exitBtn.TabIndex = 21;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(968, 468);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.netPay);
+            this.Controls.Add(this.totalDeduction);
+            this.Controls.Add(this.membershipFee);
+            this.Controls.Add(this.uifDeduction);
+            this.Controls.Add(this.payeDeduction);
+            this.Controls.Add(this.grossPay);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dependants);
+            this.Controls.Add(this.hoursWorked);
+            this.Controls.Add(this.contractorName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mzansi Tech Contractors Payroll System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox contractorName;
+        private System.Windows.Forms.TextBox hoursWorked;
+        private System.Windows.Forms.TextBox dependants;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox grossPay;
+        private System.Windows.Forms.TextBox payeDeduction;
+        private System.Windows.Forms.TextBox uifDeduction;
+        private System.Windows.Forms.TextBox membershipFee;
+        private System.Windows.Forms.TextBox totalDeduction;
+        private System.Windows.Forms.TextBox netPay;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
